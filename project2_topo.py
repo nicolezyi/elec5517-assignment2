b0e9e5c199c96f992641d99dce813b68d4ce74c6
lilywub <57131767+lilywub@users.noreply.github.com>
2019-11-04 23:12:12 -0800

    Add files via upload

--- project2_topo.py
+++ project2_topo.py
@@ -32,13 +32,17 @@ class Tree( Topo ):
         h7 = self.addHost('h7')
         h8 = self.addHost('h8')
         h9 = self.addHost('h9')
+        h10 = self.addHost('h10')
 
 
 	
-	self.addLink( s1, s2 )
-	self.addLink( s1, s3 )
-	self.addLink( s1, s4 )
-	self.addLink( s2, h1 )
+	self.addLink( s1, s2, bw=150 )
+	self.addLink( s1, s3, bw=50 )
+	self.addLink( s1, s4, bw=100 )
+        #h1s2 = {'bw':234,'delay':'25'}
+        self.addLink(h1, s2)
+
+	#self.addLink( s2, h1 )
 	self.addLink( s2, h2 )
 	self.addLink( s2, h3 )
 	self.addLink( s3, h4 )
@@ -47,7 +51,7 @@ class Tree( Topo ):
 	self.addLink( s4, h7 )
 	self.addLink( s4, h8 )
 	self.addLink( s4, h9 )
-
+        self.addLink( s1, h10)
 
 
 topos = { 'project2_topo': ( lambda: Tree() ) }
